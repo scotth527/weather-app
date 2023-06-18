b8a6360b9dd055a8a4d1e5171d5cd8f53e785f1d
Scott Huang <shuang@milestechnologies.com>
2023-06-18 14:12:15 -0400

    tested weather api

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+
+const WEATHERAPI_BASE_ENDPOINT = 'http://api.weatherapi.com/v1/';
 console.log('Hello Worlds');
+const API_KEY = '';
+
+class WeatherApp {
+    
+    constructor() {
+        console.log("This is working!!!");
+        this.get_weather('Miami');
+    }
+
+    get_weather(location, key) {
+        fetch(`${WEATHERAPI_BASE_ENDPOINT}forecast.json?key=${API_KEY}&q=${location}`, {
+            mode: 'cors'
+        })
+            .then(function(response) {
+                return response.json();
+                // Successful response :)
+            })
+            .then(function(response) { 
+                console.log("Response", response);
+            })
+            .catch(function(err) {
+                // Error :(
+                console.log("Error", err);
+            });
+    }
+
+
+
+
+    
+
+    
+
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let weatherApp = new WeatherApp();
+});

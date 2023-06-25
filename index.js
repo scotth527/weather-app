471dc8dd7be03e1dc33052abe992b22a70da8ca8
Jon Salamat <jsalamat@milestechnologies.com>
2023-06-25 19:19:05 -0400

    added search field for location and display weather data

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const WEATHERAPI_BASE_ENDPOINT = 'http://api.weatherapi.com/v1/';
 console.log('Hello Worlds');
 const API_KEY = '';
 
+let CurrentLocation;
+let CurrentCloud;
+let Condition;
+let Temp_C;
+let Temp_F;
+let Weather_Img;
+
 class WeatherApp {
     
     constructor() {
         console.log("This is working!!!");
-        this.get_weather('Miami');
+        this.get_weather(CurrentLocation);
     }
 
     get_weather(location, key) {
@@ -20,23 +27,38 @@ class WeatherApp {
             })
             .then(function(response) { 
                 console.log("Response", response);
+                CurrentLocation = "Location: " + response.location.name;
+                CurrentCloud = "Cloud: " + response.current.cloud + "%";
+                Condition= response.current.condition.text;
+                Temp_C= response.current.temp_c + "°C";
+                Temp_F= response.current.temp_f + "°F";
+                Weather_Img = response.current.condition.icon;
+                console.log(CurrentCloud);
+                console.log(Condition);
+                console.log(Temp_C);
+                console.log(Temp_F);
+                document.querySelector(".city").innerText = CurrentLocation;
+                document.querySelector(".cloud").innerText = CurrentCloud;
+                document.querySelector(".temp_C").innerText = Temp_C;
+                document.querySelector(".temp_F").innerText = Temp_F;
+                document.querySelector(".icon").src =
+                "http:" + Weather_Img;
             })
+
             .catch(function(err) {
                 // Error :(
                 console.log("Error", err);
-            });
+            })
+            
     }
-
-
-
-
-    
-
-    
-
 
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//     let weatherApp = new WeatherApp();
+// });
+
+document.querySelector(".search button").addEventListener("click", function () {
+    CurrentLocation = document.querySelector(".search-bar").value;
     let weatherApp = new WeatherApp();
 });
